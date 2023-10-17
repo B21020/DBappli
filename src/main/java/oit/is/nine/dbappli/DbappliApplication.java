6e84328ec9183f49171c3cb2d062c95f28622e9a
KosukeOkawa <e1b21020@oit.ac.jp>
2023-10-17 14:10:45 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/nine/dbappli/DbappliApplication.java
@@ -0,0 +1,13 @@
+package oit.is.nine.dbappli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbappliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbappliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/nine/dbappli/DbappliApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.nine.dbappli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbappliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
